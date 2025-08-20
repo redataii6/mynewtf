a12a974ac7e53990503744080c193285e9f11c80
redataii6 <45831615+redataii6@users.noreply.github.com>
2025-08-20 12:49:10 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "us-west-2"
     shared_config_files = ["/Users/redat/.aws/config"]
     shared_credentials_files = ["/Users/redat/.aws/credentials"]
 }
